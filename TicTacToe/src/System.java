2a63d84c73b2fe34440a25a9acc3b753ebba34ba
Kenny Lazaro <kenny.lazaro@sjsu.edu>
2019-11-04 14:51:46 -0800

    Changed turn implementation

--- TicTacToe/src/System.java
+++ TicTacToe/src/System.java
@@ -1,16 +1,19 @@
 
 public class System {
-	public static final boolean XTURN = false;
-	public static final boolean OTURN = true;
 	public static final int BLANK = 0;
 	public static final int XSYMBOL = 1;
 	public static final int OSYMBOL = 2;
-	private boolean turn;
+	private int turn;
 	private int[][] grid;
 	
 	public System() {
-		turn = XTURN;
+		turn = XSYMBOL;
 		grid = new int[3][3];
+		for (int x = 0; x < grid.length; x++) {
+			for (int y = 0; y < grid[x].length; y++) {
+				grid[x][y] = 0;
+			}
+		}
 	}
 	
 	public void switchTurn() {
@@ -20,7 +23,22 @@ public class System {
 			turn = XTURN;
 	}
 	
-	public boolean checkBoard() {
-		
+	public int checkBoard() {
+		if ((grid[0][0] == 1 && grid[0][1] == 1 && grid[0][2] == 1) ||
+			(grid[0][0] == 2 && grid[0][1] == 2 && grid[0][2] == 2)	) {
+				return turn;
+		}
+		else if ((grid[0][0] == 1 && grid[1][0] == 1 && grid[2][0] == 1) ||
+				(grid[0][0] == 2 && grid[1][0] == 2 && grid[2][0] == 2)	) {
+					return turn;
+		}
+		else if ((grid[0][0] == 1 && grid[1][1] == 1 && grid[2][2] == 1) ||
+				(grid[0][0] == 2 && grid[1][1] == 2 && grid[2][2] == 2)	) {
+					return turn;
+		}
+		else if ((grid[2][0] == 1 && grid[1][1] == 1 && grid[0][2] == 1) ||
+				(grid[2][0] == 2 && grid[1][1] == 2 && grid[0][2] == 2)	) {
+					return turn;
+		}
 	}
 }
