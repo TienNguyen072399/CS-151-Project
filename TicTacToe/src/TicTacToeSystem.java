6fcdfa319844ed32116c62edd0030a82b8aa6196
tiennguyen072399 <Tien.nguyen072399git config --global user.email Tien.nguyen072399@gmail.com>
2019-11-29 11:58:36 -0800

    Finished tic tac board, except for wordings, and started working on the board connection to the system. System was rename to TicTacToeSystem

--- TicTacToe/src/TicTacToeSystem.java
+++ TicTacToe/src/TicTacToeSystem.java
@@ -1,13 +1,15 @@
 
-public class System {
+public class TicTacToeSystem {
 	public static final int BLANK = 0;
 	public static final int XSYMBOL = 1;
 	public static final int OSYMBOL = 2;
 	private int turn;
 	private int[][] grid;
+	private boolean gameOver;
 	
-	public System() {
+	public TicTacToeSystem() {
 		turn = XSYMBOL;
+		gameOver = false;
 		grid = new int[3][3];
 		for (int x = 0; x < grid.length; x++) {
 			for (int y = 0; y < grid[x].length; y++) {
@@ -15,30 +17,54 @@ public class System {
 			}
 		}
 	}
-	
-	public void switchTurn() {
-		if (turn == XTURN)
-			turn = OTURN;
-		else
-			turn = XTURN;
+	public void setGrid(int row, int col, int symbol) {
+		grid[row][col] = symbol;
+		System.out.println("grid[" + row + "][" + col + "] = " + grid[row][col]);
 	}
 	
-	public int checkBoard() {
+	public void switchTurn() {
+		if (turn == XSYMBOL)
+			turn = OSYMBOL;
+		else
+			turn = XSYMBOL;
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public boolean getGameOver() {
+		return gameOver;
+	}
+	
+	public int checkGrid() {
+		
+		int winner = 0;
+		
 		if ((grid[0][0] == 1 && grid[0][1] == 1 && grid[0][2] == 1) ||
 			(grid[0][0] == 2 && grid[0][1] == 2 && grid[0][2] == 2)	) {
-				return turn;
+				winner = turn;
 		}
 		else if ((grid[0][0] == 1 && grid[1][0] == 1 && grid[2][0] == 1) ||
 				(grid[0][0] == 2 && grid[1][0] == 2 && grid[2][0] == 2)	) {
-					return turn;
+			winner = turn;
 		}
 		else if ((grid[0][0] == 1 && grid[1][1] == 1 && grid[2][2] == 1) ||
 				(grid[0][0] == 2 && grid[1][1] == 2 && grid[2][2] == 2)	) {
-					return turn;
+			winner = turn;
 		}
 		else if ((grid[2][0] == 1 && grid[1][1] == 1 && grid[0][2] == 1) ||
 				(grid[2][0] == 2 && grid[1][1] == 2 && grid[0][2] == 2)	) {
-					return turn;
+			winner = turn;
+		}
+		
+		
+		if(winner != 0) {
+			gameOver = true;
+			return winner;
+		}
+		else {
+			return winner;
 		}
 	}
 
